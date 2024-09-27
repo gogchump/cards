c8a16220fdd91bc7457eaf2809f98459654b87e8
gogchump <nazarovm420@gmail.com>
2024-09-27 19:26:34 +0300

    update27.09

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-document.addEventListener("DOMContentLoaded"), function(){
-let input = document. querySelector(".input")
+document.addEventListener("DOMContentLoaded", function(){
+let input = document.querySelector(".input")
+
+input.addEventListener("blur", function() {
+    let cards = Array(input.value).fill().map((e, i) => i + 1)
+
+    console.log(cards)
+} )
 
 
 
-}
+
+
+})

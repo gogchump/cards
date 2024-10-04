e429cd1f4d87a59893d17f2e852277e96a19d5aa
Zidra58 <ilumon2445@mail.ru>
2024-10-04 19:30:06 +0300

    make 04.10
    cards and game button

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(){
-    let button = document.querySelector("btn")
+    let btn = document.querySelector(".btn")
+    let input = document.querySelector("input")
+    let page = document.querySelector(".window")
+    let cards = document.querySelector(".card")
+
+    btn.addEventListener("click", function(){
+        console.log(123)
+        page.classList.remove("none")
+        list.push(input.value)
+        addElement(input.value, list)
+    })
+
+    function addElement() {
+        let card = document.addElement("div")
+
+        
+    }
 })

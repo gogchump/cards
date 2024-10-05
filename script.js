4aceb5cb1798e0a79eebda47991f1c97612b0b5a
gogchump <nazarovm420@gmail.com>
2024-10-05 03:19:44 +0300

    система создания массива

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 document.addEventListener("DOMContentLoaded", function(){
 let input = document.querySelector(".input")
-
+let list;
 input.addEventListener("blur", function() {
-    let cards = Array(input.value).fill().map((e, i) => i + 1)
+    let cards = new Array()
+    for (let i = 1; i <= input.value/2; i++)
+    {
+        cards.push(i);
+        
+    }
+    let cards1 = cards
+    cards = cards.concat(cards1)
+    
+    
+    let main = document.createElement("div")
+    main.classList.add("list")
+    document.body.appendChild(main)
 
-    console.log(cards)
+    for (let i = 1; i <= cards.length; i++) {
+        let card = document.createElement('span');
+        let span = document.createElement('p');
+        card.textContent = i;
+        span.appendChild(card);
+        
+        main.appendChild(span);
+    }
+   
+    
+
+
 } )
+
 
 
 

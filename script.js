9f3da3e482c3ef2d050f80837046b01d8f79db83
gogchump <nazarovm420@gmail.com>
2024-10-06 01:46:08 +0300

    update js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
 let input = document.querySelector(".input")
 
+
 input.addEventListener("blur", function() {
     let cards = new Array()
     for (let i = 1; i <= input.value/2; i++)
@@ -16,20 +17,35 @@ input.addEventListener("blur", function() {
     main.classList.add("list")
     document.body.appendChild(main)
 
-    for (let i = 1; i <= cards.length; i++) {
-        let card = document.createElement('span');
-        let span = document.createElement('p');
-        card.textContent = i;
-        span.appendChild(card);
-        
-        main.appendChild(span);
-    }
-   
     
 
+            
+            for (let i = 1; i <= cards.length; i++) {
 
+                
+                let card = document.createElement('span');
+                let span = document.createElement('p');
+
+
+                let random = getRandomElement(cards);
+                card.textContent = random;
+                cards.slice(random, random)
+                
+                
+
+                span.appendChild(card);
+                main.appendChild(span);
+            }
+    
+    
+    
+    
+    
 } )
 
+function getRandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
 
 
 

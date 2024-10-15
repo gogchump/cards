6dbb26d81dbd6ef9a4d730b6378141f6be96cfb3
gogchump <nazarovm420@gmail.com>
2024-10-15 23:19:32 +0300

    release

--- script.js
+++ script.js
@@ -1,54 +1,100 @@
 document.addEventListener("DOMContentLoaded", function(){
-let input = document.querySelector(".input")
-
-
-input.addEventListener("blur", function() {
-    let cards = new Array()
-    for (let i = 1; i <= input.value/2; i++)
-    {
-        cards.push(i);
-        
-    }
-    let cards1 = cards
-    cards = cards.concat(cards1)
+    let input = document.querySelector(".input")
     
     
-    let main = document.createElement("div")
-    main.classList.add("list")
-    document.body.appendChild(main)
-
-    
-
+    input.addEventListener("blur", function() {
+        let cards = new Array()
+        for (let i = 1; i <= input.value/2; i++)
+        {
+            cards.push(i);
+            cards.push(i);
             
-            for (let i = 1; i <= cards.length; i++) {
-
+        }
+    
+        // let cards1 = cards
+        // cards = cards.concat(cards1)
+        console.log(cards);
+        shuffle(cards);
+        console.log(cards);
+        
+        let main = document.createElement("div")
+        main.classList.add("list")
+        document.body.appendChild(main)
+        let arr = []
+        let id = []
+        let victory = []
+        for (let i = 0; i <= cards.length-1; i++) {
+            
+            let card = document.createElement('div');
+            let text = document.createElement('p');
+            text.textContent = cards[i];
+            card.classList.add("card");
+            text.classList.add("none"); 
+            
+    
+            
+            card.addEventListener("click", function(e){
+                arr.push(e.target.textContent)
+                console.log(arr)
+                text.classList.remove("none"); 
+                e.disabled = true;
+                id.push(text)
+    
                 
-                let card = document.createElement('span');
-                let span = document.createElement('p');
-
-
-                let random = getRandomElement(cards);
-                card.textContent = random;
-                cards.slice(random, random)
+                if(arr.length == 2) {
+                    console.log(arr)
+                    if(arr[0] == arr[1]){
+                        console.log("123")
+                        arr.pop(0);
+                        arr.pop(0);
+                        console.log(arr)
+                        console.log(id)
+                        id.pop(0);
+                        id.pop(0);
+                        victory.push("1")
+                        if(victory.length == cards.length/2) 
+                            {
+                                console.log("victory")
+                            }  
+                    
+                    }
+                    else {
+                        setTimeout(() => {
+                            console.log("321")
+                            arr.pop(0);
+                            arr.pop(0);
+                            id[1].classList.add("none")
+                            id[0].classList.add("none")   
+                            id.pop(0);
+                            id.pop(0);
+                        }, 500);
+                         }
+                    
+                        
+                }
                 
-                
-
-                span.appendChild(card);
-                main.appendChild(span);
             }
+        )
+            
+            
+            card.appendChild(text);
+            main.appendChild(card);
+    
+        }
+        
+        
+        
+        
+        
+    } )
+    
+    function shuffle(array) {
+        array.sort(() => Math.random() - 0.5);
+      }
     
     
     
     
     
-} )
-
-function getRandomElement(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-}
-
-
-
-
-
-})
+    })
+    
